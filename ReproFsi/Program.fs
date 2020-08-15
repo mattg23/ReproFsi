f2f1e018121eba82d4b1b4830c6c140e56b5dfd0
Matthias Grunewald <matthias.grunewald@gritplex.com>
2020-08-15 13:53:28 +0200

    Add project files.

---
+++ ReproFsi/Program.fs
@@ -0,0 +1,40 @@
+﻿open System.Text
+open System.IO
+open FSharp.Compiler.Interactive.Shell
+
+let smallLambda = fun x -> let p = x
+                           p
+
+let code = """
+let smallLambda = fun x -> let p = x
+                           p
+"""
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "try compiling %A" smallLambda
+
+    let sbOut = StringBuilder()
+    let sbErr = StringBuilder()
+
+    let fsi =
+        let inStream = new StringReader("")
+        let outStream = new StringWriter(sbOut)
+        let errStream = new StringWriter(sbErr)
+        let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()       
+        let argv = [| "fsi.exe"; "--noninteractive"; "--quiet";"--nologo";"--langversion:latest" |]
+        let fsi_noloaded = FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream)
+        fsi_noloaded.EvalInteractionNonThrowing "open System;;" |> ignore
+        fsi_noloaded.EvalInteractionNonThrowing "#indent \"off\";;" |> ignore
+        fsi_noloaded
+
+    let evalresult, errors = fsi.EvalExpressionNonThrowing(code)
+
+    match evalresult with
+    | Choice1Of2(Some fsiValue) -> printfn "compile successful: %A" fsiValue
+    | Choice2Of2(e) -> printfn "%A" e
+
+    errors |> Array.map (printfn "%A") |> ignore
+
+    0 // return an integer exit code
